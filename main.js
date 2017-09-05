527e85ac89697e369c0ca3271c372b4b4ae89bb1
Didier Roy <didier@d2r2.net>
2017-09-04 20:38:34 -0400

    ajout système de assets

--- main.js
+++ main.js
@@ -3,20 +3,44 @@ const http =   require('http'),
     fs     =   require('fs'),
     mosca  =   require('mosca'),
     mqttServ  = new mosca.Server({}),
-    router    = require('router');
+    finalhandler = require('finalhandler'),
+    Router    = require('router'),
+    router   = Router();
+
+
+fs.readdir('./public/assets', (err,folders) => {
+  folders.forEach(folder => {
+    fs.readdir('./public/assets/' + folder, (err,files) => {
+      files.forEach(file => {
+        router.get('/a/' + folder + '/' + file,(req,res) => {
+          res.writeHead(200,{'Content-Type':'text/javascript'});
+          const assetUrl = fs.readFileSync('./public/assets/' + folder + '/' + file);
+          res.write(assetUrl);
+          res.end();
+        });
+      });
+    });
+  });
+});
 
 
 
-const httpServ = http.createServer(function (req,res) {
+router.get('/',(req,res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   const html = fs.readFileSync('./public/index.html');
   res.write(html);
   res.end();
 });
 
+
+
+const httpServ = http.createServer((req,res) => {
+router(req,res,finalhandler(req,res));
+});
+
 mqttServ.attachHttpServer(httpServ);
 
-mqttServ.on('clientConnected', function(client) {
+mqttServ.on('clientConnected', (client) => {
 	console.log('client connected', client.id);
 });
 
