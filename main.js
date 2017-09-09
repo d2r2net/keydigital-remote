05153ffb364fa7d6f1296de0ec76235b5e3c3c51
Didier Roy <didier@d2r2.net>
2017-09-09 17:46:58 -0400

    migration vers express o.k avec socket mqtt

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
 "use strict";
-const http = require('http'),
-  fs = require('fs'),
+const express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
   cmd = require('./config/cmd'),
   mosca = require('mosca'),
-  mime = require('mime'),
   mqttServ = new mosca.Server({}),
-  finalhandler = require('finalhandler'),
-  nunjucks = require('nunjucks'),
-  Router = require('router'),
-  router = Router();
-
+  nunjucks = require('nunjucks');
+//express config
+app.use('/a',express.static('./plublic/assets'));
 //mqtt stuff
-const httpServ = http.createServer((req, res) => {
-  router(req, res, finalhandler(req, res));
-});
-
-mqttServ.attachHttpServer(httpServ);
+mqttServ.attachHttpServer(server);
 
 mqttServ.on('clientConnected', (client) => {
   console.log('client connected', client.id);
 });
-// webserver stuff
-// static assets 
-fs.readdir('./public/assets', (err, folders) => {
-  folders.forEach(folder => {
-    fs.readdir('./public/assets/' + folder, (err, files) => {
-      files.forEach(file => {
-        router.get('/a/' + folder + '/' + file, (req, res) => {
-          const assetPath = './public/assets/' + folder + '/' + file;
-          const assetFile = fs.readFileSync('./public/assets/' + folder + '/' + file);
-          const assetMime = mime.lookup(assetPath);
-          res.writeHead(200, {
-            'Content-Type': assetMime
-          });
-          res.write(assetFile);
-          res.end();
-        });
-      });
-    });
-  });
-});
 
-router.get('/', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  const html = fs.readFileSync('./public/index.html');
-  res.write(html);
-  res.end();
-});
 
-httpServ.listen(3000);
+app.get('/',(req,res) => {
+  console.log('tadam!');
+  res.send('Hello World!');
+});
+server.listen(3000);
