b05ea8349c4a0881a2cf968c261687b7e6df9333
Didier Roy <didier@d2r2.net>
2017-09-28 10:10:46 -0400

    mise en place mqtt frontend et channels
    validation ok reste plus que backend

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -2,7 +2,8 @@
 
 var client = mqtt.connect();
 
-  client.subscribe("mqtt/demo");
+  client.subscribe("outputs/out1");
+
 
   client.on("message", function(topic, payload) {
   let pp = [topic, payload].join(": ");
@@ -14,9 +15,18 @@ var client = mqtt.connect();
 
   $( document ).ready(function() {
       $('ons-button').click(function(e) {
-    const pId = $('ons-page').attr('id');
-        const idClicked = e.target.id;
-
-        console.log(pId + " " + idClicked + " button clicked!.");
+    const idClicked = e.target.id;
+    console.log(idClicked);
+    validateClicked(idClicked);
       });
+
   });
+function validateClicked(clicked) {
+  let clickedOut = clicked.substring(0, clicked.indexOf(':'));
+  let clickedIn = clicked.substring(clicked.indexOf(':') +1);
+  console.log(clickedOut);
+  console.log(clickedIn);
+  if (clickedOut.match(/^out([0-9]+)$/) && clickedIn.match(/^in([0-9]+)$/)){
+    client.publish("outputs/" + clickedOut,clickedIn);
+  }
+}
