5a995df9fa2d509dee7845b12e25c4d1551f041f
Didier Roy <didier@d2r2.net>
2017-09-29 09:50:24 -0400

    client serveur mqtt
    les channels subscribe ne son pas encore auto

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   cmd = require('./config/cmd'),
+  mqtt = require('mqtt'),
   mosca = require('mosca'),
   mqttServ = new mosca.Server({}),
-  nunjucks = require('nunjucks');
+  nunjucks = require('nunjucks'),
+  SerialPort = require('serialport'),
+  outputs = cmd.outputs,
+  inputs = cmd.inputs;
 
 //nunjucks config
 nunjucks.configure('views', {
@@ -17,7 +21,7 @@ nunjucks.configure('views', {
 //express config
 app.use('/a', express.static('public/assets'));
 
-//mqtt stuff
+//mqtt server  stuff
 mqttServ.attachHttpServer(server);
 
 mqttServ.on('clientConnected', (client) => {
@@ -29,8 +33,8 @@ app.get('/',(req,res) => {
   console.log('tadam!');
   var data = {bonjour:'allo',
   serveur:cmd.serveur.url,
-  inputs:cmd.inputs,
-  outputs:cmd.outputs};
+  inputs:inputs,
+  outputs:outputs};
 
 
   res.render('index.html',data);
@@ -38,3 +42,35 @@ app.get('/',(req,res) => {
 
 });
 server.listen(3000);
+
+//mqtt client stuff
+var client  = mqtt.connect('ws://127.0.0.1:3000')
+
+client.on('connect', function () {
+  client.subscribe('outputs/out1');
+  client.subscribe('outputs/out2');
+
+
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(topic.toString() + ' ' + message.toString());
+  
+});
+// const client = mqtt.connect('mqtt://127.0.0.1:3000');
+//
+// client.on('connect',() => {
+//   Object.keys(cmd.outputs).forEach((key) => {
+//     var out = outputs[key];
+//     console.log('la key: ' + key);
+//     if (out.active === true){
+//       client.subscribe('outputs/' + out);
+//     }
+//   });
+// });
+//
+//   client.on('message',(topic,payload) => {
+//         console.log(topic.toString + " " + payload.toString);
+//         client.end();
+//   });
